a6f1d1c0ae9ebfb8143d2b67cb1eb2f9502f3a35
pierre <pierrerohel@gmail.com>
2014-11-20 16:45:44 -0500

    fixing

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
 
   resources :trips
 
-  devise_scope :user do
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
